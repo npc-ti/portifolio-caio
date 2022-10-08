a5b5633318d557feaaeafe93a224a914bdd8a7db
Caio Caik Fresneda de Souza <caiofresneda@gmail.com>
2022-10-07 22:43:03 -0300

    added Hero Section

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: NextPage = () => {
   return <>
+  
   <Head>
     <title>Portifolio</title>
   </Head>
@@ -12,7 +14,9 @@ const Home: NextPage = () => {
   <Header/>
 
   {/* Hero */}
-
+  <section id="Hero">
+    <Hero/>
+  </section>
   {/* About */}
 
   {/* Experience */}
