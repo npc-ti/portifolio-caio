72528b13685f9eca11b2e84b61fb2f79670f1f4c
Caio Caik Fresneda de Souza <caiofresneda@gmail.com>
2022-10-09 04:01:02 -0300

    created about config

---
+++ web/config/about.ts
@@ -0,0 +1,10 @@
+export interface aboutProps {
+    Resume: string,
+}
+
+const aboutConfig: aboutProps = {
+    Resume: "Olá, me chamo Caio! Sou um desenvolvedor Full Stack, com mais de 3 anos de experiencia em realizações de aplicações de pequeno e médio porte utilizando tecnologias como, ReactJs, NextJs, NodeJs, Sql, Python entre outras. Amo estudar tecnologia, não seria um eufemismo falar que eu vivo pela e por tecnologia e adoro a utilizar ela para resolver problemas. "
+
+}
+
+export default aboutConfig;
