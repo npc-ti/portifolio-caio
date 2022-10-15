ae9f2e8209255f90f0cf1a76648c0db88aacb680
cks12 <caiofresneda@gmail.com>
2022-10-15 03:39:31 +0000

    fix bugs

--- web/components/About.tsx
+++ web/components/About.tsx
@@ -8,9 +8,10 @@ export default function About() {
       initial={{opacity:0}}
       whileInView = {{opacity:1}}
       transition ={{duration:1.5}}
-      className='container md:text:left md:flex-row max-w-7xl px-10 justify-evenly mx-auto'>
+      className='container md:text:left max-w-7xl px-10 justify-evenly mx-auto'>
       <h3 className="title">
         Um pouco sobre mim</h3>
+        <div className="flex flex-col md:flex-row items-center">
 
         <motion.img 
           initial = {{
@@ -21,7 +22,7 @@ export default function About() {
             duration:1.2
           }}
           viewport={{once: true}}
-          className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 
+          className="md:mb-0 flex-shrink-0 w-56 h-56 
           rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
           whileInView={{opacity:1, x:0}}
           src={"/assets/images/Caio-about.jpeg"}/>
@@ -31,6 +32,8 @@ export default function About() {
             <p className='text-base'>
               {aboutConfig.Resume}
             </p>
+        </div>
+
           </div>
     </motion.div>
   )
