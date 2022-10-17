36215be1d6894e00fcf6d5837eab69bf5b5bc2eb
cks12 <caiofresneda@gmail.com>
2022-10-17 22:24:18 +0000

    Fix bugs, add comments and add Contact section

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next';
+
+// Import navbar
 import Head from 'next/head';
+
+// Imports Pages
 import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import Work from '../components/Work';
+import Contact from '../components/Contact';
 
 const Home: NextPage = () => {
   return <>
@@ -20,36 +25,38 @@ const Home: NextPage = () => {
 
   {/* Hero */}
   <section 
-    id="resume">
+    id="resume" className='snap-start'>
     <Hero/>
   </section>
 
   {/* About */}
-  <section id="sobre">
+  <section id="sobre" className='snap-start'>
     <About/>
     </section>
 
   {/* Experience */}
 
-  <section id="experiencia">
+  <section id="experiencia" className='snap-start'>
     <Work/>
     </section>
 
   {/* Skills */}
 
-  <section id="skills">
+  <section id="skills" className='snap-start'>
     <Skills/>
   </section>
 
 
   {/* Projects */}
 
-  <section id="projetos">
+  <section id="projetos" className='snap-center'>
     <Projects/>
   </section>
 
   {/* Contact Me */}
-
+  <section id="contact" className='snap-start'>
+    <Contact/>
+    </section>
     </div> 
   </>
 }
